a9349fff74b27df3574a3d70d8183da4eda1047d
Edward <wardvisual@gmail.com>
2021-12-21 13:20:19 +0800

    Fix and remove unnecessary codes

--- src/js/background.js
+++ src/js/background.js
@@ -1,35 +1,22 @@
 import "../images/icon-128.png";
 import "../images/icon-34.png";
 
-class Background {
-    default () {
-        chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-            alert("ACTIVE 1");
-
-            if (changeInfo.status === "loading" && tab.active) {
-                alert("ACTIVE 2");
-                this.getBody();
-            }
-        });
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.status === "loading" && tab.active) {
+        loadBody();
     }
+});
 
-    async getBody() {
-        await chrome.tabs.query({ active: true }, function(tabs) {
-            await chrome.storage.sync.get("urlInput", (data) => {
-                alert("getBody", data.urlInput);
-                this.redirectUrl(tabs, data.urlInput);
-            });
+const redirectUrl = (tabs, oldUrl) => {
+    chrome.tabs.update(tabs[0].id, {
+        url: `https://12ft.io/proxy?q=${oldUrl.toString()}`,
+    });
+};
+
+const loadBody = () => {
+    chrome.tabs.query({ active: true }, (tabs) => {
+        chrome.storage.sync.get("urlInput", (data) => {
+            redirectUrl(tabs, data.urlInput);
         });
-    }
-
-    redirectUrl(tabs, oldUrl) {
-        chrome.tabs.update(tabs[0].id, {
-            url: `https://12ft.io/${oldUrl.toString()}`,
-        });
-    }
-}
-
-const bg = new Background();
-
-alert("BG RUNNING");
-bg.default();
+    });
+};
